37f7e9b033ea360450a3e4cb66bf50100bb2cc9a
Ziah <zvanblerck1@gmail.com>
2026-07-16 12:54:59 +0200

    added some mock icons

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -11,16 +11,13 @@ var camera_sens = 50
 var capMouse = false
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -43,6 +40,7 @@ func _physics_process(delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion: 
 		look_dir = event.relative * 0.01
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _rotate_camera(delta: float, sens_mod:float = 1.0):
 	rotation.y -= look_dir.x * camera_sens * delta
